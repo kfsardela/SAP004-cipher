74be1f5574a2a3225634ba937e1a25e174332d23
Karine Sardela <kfsardela@gmail.com>
2020-04-06 19:28:54 -0300

    Criação das funcionalidades da pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let botao1 = document.getElementById("botaoCifrar");
+let botao2 = document.getElementById("botaoDecifrar");
+
+function encode () {
+    let msgDigitada = document.getElementById("mensagemDigitada").value;
+    let deslocamento = parseInt(document.getElementById("deslocamento").value);
+    
+    let encodeMsg = cipher.encode(deslocamento, msgDigitada)
+
+    document.getElementById("codificacao").innerText = "cifrada"  
+    document.getElementById("novaMsg").innerText = encodeMsg;
+}
+botao1.addEventListener('click', encode)
+
+function decode () {
+    let msgDigitada = document.getElementById("mensagemDigitada").value;
+    let deslocamento = parseInt(document.getElementById("deslocamento").value);
+    
+    let decodeMsg = cipher.decode(deslocamento, msgDigitada)
+
+    document.getElementById("codificacao").innerText = "decifrada"  
+    document.getElementById("novaMsg").innerText = decodeMsg;
+   
+  }
+  botao2.addEventListener('click', decode)
+
+
